5014cd60118156bbcc5476d84580c554942aba68
Alden Timme <alden.timme@gmail.com>
2012-12-19 16:31:34 -0800

    added setup.sh script

---
+++ setup.sh
@@ -0,0 +1,46 @@
+# Consider making this a README.md
+
+# create user `aotimme`
+sudo useradd -d /home/aotimme -m aotimme
+sudo passwd aotimme
+# add to sudoers
+sudo adduser aotimme sudo
+
+### Change to being aotimme
+
+# * add Host to local ~/.ssh/config
+# * add local id_rsa.pub to ~/.ssh/authorized_keys
+# * now can `ssh host` w/o password
+
+# install git, git-flow, zsh, npm (may need to update/upgrade?)
+sudo apt-get install git git-flow zsh npm
+
+# configure git
+git config --global user.email "alden.timme@gmail.com"
+git config --global user.name "Alden Timme"
+
+# clone dotfiles
+git clone git@github.com:aotimme/dotfiles.git
+ln -s ~/dotfiles/screenrc .screenrc
+
+# install oh-my-zsh
+git clone git@github.com:robbyrussell/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/zshrc .zshrc
+# cp .oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+# * exit and ssh again
+
+git clone git@github.com:aotimme/vim ~/.vim
+ln -s ~/.vim/vimrc .vimrc
+# OR:
+# ln -s ~/dotfiles/vimrc .vimrc
+
+# set up webserver
+sudo apt-get install nginx-full
+sudo service nginx start
+
+# install mongodb
+sudo apt-get install mongodb-dev
+
+# install ruby stuff (for rake)
+sudo apt-get install ruby-rvm
